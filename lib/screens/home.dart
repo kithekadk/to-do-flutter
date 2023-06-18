711470eb9db0d790295a5139e973d557b18779f9
unknown <dankinyi99@gmail.com>
2023-06-18 20:25:53 +0300

    fully working todo app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,22 +3,81 @@ import 'package:todo_flutter/constants/colors.dart';
 import 'package:todo_flutter/model/todo.dart';
 import 'package:todo_flutter/widgets/todo_item.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<ToDo> foundToDo = [];
+
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    foundToDo = todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: tdBGColor,
         appBar: _buildAppBar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              searchBox(),
-              viewToDos(),
-            ],
-          ),
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  searchBox(),
+                  viewToDos(),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(children: [
+                Expanded(
+                    child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0)
+                      ],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: const InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none),
+                  ),
+                )),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      addToDoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: tdBlue,
+                        minimumSize: const Size(60, 60)),
+                    child: const Text('+', style: TextStyle(fontSize: 40)),
+                  ),
+                )
+              ]),
+            )
+          ],
         ));
   }
 
@@ -44,47 +103,92 @@ class Home extends StatelessWidget {
       ]),
     );
   }
-}
+
+  final todosList = ToDo.todoList();
+
+  void handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void deleteToDoItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void addToDoItem(String toDo) {
+    setState(() {
+      todosList.add(ToDo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          description: toDo));
+    });
+    _todoController.clear();
+  }
+
+  void runFilter(String enteredKeyword) {
+    List<ToDo> results = [];
+
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.description!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      foundToDo = results;
+    });
+  }
+
+  Widget viewToDos() {
+    return Expanded(
+      child: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 30, bottom: 20),
+            child: const Text(
+              'All Todos',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+            ),
+          ),
+          // for (ToDo mytodo in todosList)
+          for (ToDo mytodo in foundToDo.reversed) //to order from last todo created
+            TodoItem(
+              todo: mytodo,
+              onToDoChanged: handleToDoChange,
+              onDeleteToDo: deleteToDoItem,
+            ),
+        ],
+      ),
+    );
+  }
 
 // This is the search box
-Widget searchBox() {
-  return Container(
-    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-    decoration: BoxDecoration(
-        color: Colors.white, borderRadius: BorderRadius.circular(20)),
-    child: const TextField(
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(10),
-          prefixIcon: Icon(
-            Icons.search,
-            color: tdBlack,
-            size: 20,
-          ),
-          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
-          border: InputBorder.none,
-          hintText: 'Search',
-          hintStyle: TextStyle(color: tdGrey)),
-    ),
-  );
-}
-
-Widget viewToDos() {
-  final todosList = ToDo.todoList();
-  return Expanded(
-    child: ListView(
-      padding: const EdgeInsets.all(10),
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 40, bottom: 20),
-          child: const Text(
-            'All Todos',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-          ),
-        ),
-
-        for(ToDo mytodo in todosList)
-          TodoItem( todo: mytodo),
-      ],
-    ),
-  );
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        onChanged: (value)=> runFilter(value),
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(10),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
+  }
 }
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -4,76 +4,55 @@ import 'package:todo_flutter/model/todo.dart';
 
 class TodoItem extends StatelessWidget {
   final ToDo todo;
-  const TodoItem({super.key, required this.todo});
+  final onToDoChanged;
+  final onDeleteToDo;
+
+  const TodoItem(
+      {super.key,
+      required this.todo,
+      required this.onToDoChanged,
+      required this.onDeleteToDo});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: ListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                onTap: () {},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                tileColor: Colors.white,
-                leading: Icon(
-                  todo.isDone ? Icons.check_box : Icons.check_box_outline_blank, 
-                  color: tdBlue),
-                title: Text(
-                  // 'Cleaning the house', //This was before i started using the model to view todos
-                  todo.description!,
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: tdBlack,
-                      decoration: todo.isDone ? TextDecoration.lineThrough : null
-                  )),
-                trailing: Container(
-                  // padding: const EdgeInsets.all(10),
-                  // margin: const EdgeInsets.symmetric(vertical: 12.0),
-                  height: 35,
-                  width: 35,
-                  decoration: BoxDecoration(
-                      color: tdRed, borderRadius: BorderRadius.circular(5)),
-                  child: IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Colors.white,
-                    iconSize: 18,
-                    onPressed: () {},
-                  ),
-                )
-              )
-        ),
-
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 20,
-                    right: 20,
-                    left: 20
-                  ),
-                  decoration: const BoxDecoration(
-                    color:Colors.white,
-                    // boxShadow: BoxShadow(
-                    //   color: Colors.grey, 
-                    //   offset: Offset(0.0, 0.0),
-                    //   blurRadius: 10.0,
-                    //   spreadRadius: 0.0
-                    // )
-                  ),
-                )
-              )
-            ]
-          ),
-        )
-      ],
-    );
+    return Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        child: ListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            onTap: () {
+              onToDoChanged(todo);
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            tileColor: Colors.white,
+            leading: Icon(
+                todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+                color: tdBlue),
+            title: Text(
+                // 'Cleaning the house', //This was before i started using the model to view todos
+                todo.description!,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: tdBlack,
+                    decoration:
+                        todo.isDone ? TextDecoration.lineThrough : null)),
+            trailing: Container(
+              // padding: const EdgeInsets.all(10),
+              // margin: const EdgeInsets.symmetric(vertical: 12.0),
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                  color: tdRed, borderRadius: BorderRadius.circular(5)),
+              child: IconButton(
+                icon: const Icon(Icons.delete),
+                color: Colors.white,
+                iconSize: 18,
+                onPressed: () {
+                  onDeleteToDo(todo.id);
+                },
+              ),
+            )));
   }
 }
